bc9a254a0fa0e1ae47efcdc6afbf17bf09a14291
Simon Bensaid <lamond.tada@gmail.com>
2017-07-10 21:47:11 +0200

    madoff

---
+++ cesar.rb
@@ -0,0 +1,17 @@
+ALPHABET_SIZE = 26
+
+def caesar_cipher(string)
+  shiftyArray = []
+  charLine = string.chars.map(&:ord)
+
+  shift = 5
+  ALPHABET_SIZE.times do |shift|
+    shiftyArray << charLine.map do |c|
+      ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+    end.join
+  end
+
+  shiftyArray
+end
+
+puts caesar_cipher("Bmfy f xywnsl!")
